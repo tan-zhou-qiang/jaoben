91fe9f99de82c6b2bdf9e0951ce6d4a7ce59c07a
tanzhouqiang <1533070657@qq.com>
2022-08-17 16:57:56 +0800

    add license

---
+++ 1.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         谭周强
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  第一个脚本
+// @author       tan
+// @match        *://*.zhihu.com/*
+// @icon         none
+// @grant        none
+// ==/UserScript==
+ 
+(function() {
+    'use strict';
+    console.log("hello");
+})();
